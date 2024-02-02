0ca8fe35d387fcda6ee8c860e74f18153f1af7e2
ghislain-iteriteka <ghislainiteriteka@gmail.com>
2024-02-02 08:48:03 +0200

    second commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script >
+    import LoginView from "./components/LoginView.vue"
+    export default{
+        components:{LoginView},
+        mounted(){
+            let user = JSON.parse(localStorage.getItem("user"))
+            if(user){
+                this.$store.state.user=user
+            }
+        }
+   }
+</script>
+
+<template>
+    <router-view v-if="$store.state.user!=null" ></router-view>
+    <LoginView v-else ></LoginView>
+</template>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ListeEmployes from "../views/ListeEmployes.vue"
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path:"/",
+      name:"listeEmployes",
+      component:ListeEmployes
+    }
+  ]
+})
+
+export default router
